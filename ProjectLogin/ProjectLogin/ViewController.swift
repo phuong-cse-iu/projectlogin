2017b6103791fe50e03de6796bb5d1e2d234e649
TranTPhuong <trantphuong@Phuong-Pro.lan>
2017-02-07 08:31:43 +0700

    Second commit

--- ProjectLogin/ProjectLogin/ViewController.swift
+++ ProjectLogin/ProjectLogin/ViewController.swift
@@ -7,17 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var playerLayer:AVPlayerLayer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        playerLayer = AVPlayerLayer()
+        let url:URL = URL(string: "http://thaihuynhcong.pe.hu/Video/SING%20-%20Teaser%20Trailer.mp4")!
+        playerLayer.player = AVPlayer(url: url)
+        view.layer.addSublayer(playerLayer)
+        playerLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        playerLayer.fra
+        playerLayer.player?.play()
+        
     }
 
 
