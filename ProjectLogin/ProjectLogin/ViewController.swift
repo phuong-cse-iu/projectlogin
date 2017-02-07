78b26e5b29b3ae3c8ac5b33eaa9629fa2a2f8abd
TranTPhuong <trantphuong@Phuong-Pro.lan>
2017-02-07 08:58:10 +0700

    Login form and video background

--- ProjectLogin/ProjectLogin/ViewController.swift
+++ ProjectLogin/ProjectLogin/ViewController.swift
@@ -16,11 +16,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         playerLayer = AVPlayerLayer()
-        let url:URL = URL(string: "http://thaihuynhcong.pe.hu/Video/SING%20-%20Teaser%20Trailer.mp4")!
+//        let url:URL = URL(string: "http://thaihuynhcong.pe.hu/Video/SING%20-%20Teaser%20Trailer.mp4")!
+        
+        let path = Bundle.main.path(forResource: "spiderman", ofType: "mp4")
+        
+        let url:URL = URL(fileURLWithPath: path!)
         playerLayer.player = AVPlayer(url: url)
-        view.layer.addSublayer(playerLayer)
+        view.layer.insertSublayer(playerLayer, at: 0)
         playerLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
-        playerLayer.fra
+        playerLayer.videoGravity = AVLayerVideoGravityResize
         playerLayer.player?.play()
         
     }
